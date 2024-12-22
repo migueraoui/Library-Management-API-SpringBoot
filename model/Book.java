d4a4e1d49154b8c64061ab3be175bfaf2acb527e
migueraoui <mohamedilyesgueraoui@gmail.com>
2024-12-22 19:41:08 +0100

    add getters and setters

--- model/Book.java
+++ model/Book.java
@@ -15,4 +15,44 @@ public class Book {
     private String author;
     private String genre;
     private int copiesAvailable;
+
+        public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+
+    public void setCopiesAvailable(int copiesAvailable) {
+        this.copiesAvailable = copiesAvailable;
+    }
 }

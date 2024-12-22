c589fa15fee7dbe9356bf837ebd9dc5a4ee55577
migueraoui <mohamedilyesgueraoui@gmail.com>
2024-12-22 19:53:21 +0100

    Create BorrowRequest and ReturnRequest Classes

---
+++ request/BorrowRequest.java
@@ -0,0 +1,23 @@
+package sp.migr.librarymanagement.request;
+
+public class BorrowRequest {
+
+    private Long userId;
+    private Long bookId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+}

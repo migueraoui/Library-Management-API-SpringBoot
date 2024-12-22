5e5822981bdf1c361f3152bf8ef43c2d86d8237b
migueraoui <mohamedilyesgueraoui@gmail.com>
2024-12-22 19:59:08 +0100

    add necessary methods

--- repository/TransactionRepository.java
+++ repository/TransactionRepository.java
@@ -5,4 +5,6 @@ import sp.migr.librarymanagement.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    @Query("SELECT t FROM Transaction t WHERE t.userId = :userId AND t.bookId = :bookId AND t.status = 'BORROWED'")
+    Transaction findByUserIdAndBookId(Long userId, Long bookId);
 }
